d8f3282452c9683ad0ec6767f9a264bec27715d8
lopusy <lopusy@gmail.com>
2023-01-24 23:34:03 +0100

    dodanie funkcji split_dataset

--- data.py
+++ data.py
@@ -34,7 +34,44 @@ def load_file(path, header=False,start=None,stop=None ):
             if(start is None or int(start)<= i) and(stop is None or i < int(stop)):
                 print(row)
         csvfile.close()  
+
+#Funkcja która dzieli dataset na część treningową, testową i walidacyjną
+#-----------------------------------------------------------------------
+def split_dataset(dataset, train_percent, val_percent, test_percent):
+    def load_csv(path):
+        import csv 
+        with open(path, 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            return list(reader)
         
+    dataset = load_csv(path)
+    # obliczamy sumę procentów
+    total_percent = train_percent + val_percent + test_percent
+    
+    # sprawdzamy, czy suma procentów jest równa 100
+    if total_percent != 100:
+        raise ValueError("Procenty nie dają sumy 100")
+    
+    # obliczamy liczbę próbek w każdym podzbiorze
+    train_size = int(len(dataset) * train_percent / 100)
+    val_size = int(len(dataset) * val_percent / 100)
+    test_size = int(len(dataset) * test_percent / 100)
+    
+    # dzielimy zbiór na podzbiory
+    train_set = dataset[:train_size]
+    val_set = dataset[train_size:train_size+val_size]
+    test_set = dataset[train_size+val_size:]
+    
+    # zwracamy podzbiory jako tuple
+    print('\nzbiór treningowy:')
+    print(train_size)
+    print('zbiór walidacyjny:')
+    print(val_size)
+    print('zbiór testowy:')
+    print(test_size)
+    return train_set, val_set, test_set
+    
+     
 #-----------------Przykad użycia---------------------------------------------------
 #----------------------------------------------------------------------------------
 
@@ -44,3 +81,6 @@ path = 'zmodyfikowany.csv'
 load_data(plik) 
 # 2.  ładujemy dane z etykietami lub bez
 load_file(path, header=False, start=0, stop=10)
+# 3. dzielimy dataset na cześć treningową i testową:
+
+split_dataset(path,70,0,30)  
